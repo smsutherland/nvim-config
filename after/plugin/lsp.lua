f7f453d77d50c428731b0fe37953ef3fd5bd03f1
smsutherland <sagan@farthinghalearms.com>
2023-07-16 14:37:29 -0400

    basic lsp

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,10 @@
+local lsp = require("lsp-zero").preset({})
+lsp.on_attach(function(client, bufnr)
+	lsp.default_keymaps({buffer = bufnr})
+end)
+
+lsp.setup_servers({"rust_analyzer"})
+
+require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
+
+lsp.setup()
--- lua/sagan/lazy.lua
+++ lua/sagan/lazy.lua
@@ -72,4 +72,14 @@ require("lazy").setup({
 			"nvim-lua/plenary.nvim",
 		},
 	},
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v2.x",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"hrsh7th/nvim-cmp",
+			"hrsh7th/cmp-nvim-lsp",
+			"L3MON4D3/LuaSnip",
+		},
+	},
 }, {})
