f99a66725aac1b84f6e4c1285fde471a1bfaeda4
smsutherland <sagan@farthinghalearms.com>
2023-10-26 12:35:57 -0400

    spellcheck latex files

--- lua/sagan/autocmd.lua
+++ lua/sagan/autocmd.lua
@@ -25,6 +25,8 @@ autocmd({ "BufRead, BufNewFile" }, {
     callback = function()
         vim.opt_local.wrap = true
         vim.opt_local.linebreak = true
+        vim.opt_local.spell = true
+        vim.opt_local.spelllang = "en_us"
     end,
 })
 
