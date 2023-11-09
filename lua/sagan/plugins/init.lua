618a5667f43b7ae0defc3a5a3e740559d9fc7235
smsutherland <sagan@farthinghalearms.com>
2023-11-09 13:47:19 -0500

    lsp progress on lualine

--- lua/sagan/plugins/init.lua
+++ lua/sagan/plugins/init.lua
@@ -250,10 +250,16 @@ return {
     {
         "nvim-lualine/lualine.nvim",
         event = "BufEnter",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
+        dependencies = { "nvim-tree/nvim-web-devicons", "arkav/lualine-lsp-progress" },
         opts = {
             options = {
                 theme = "catppuccin",
+            },
+            sections = {
+                lualine_c = {
+                    "filename",
+                    "lsp_progress",
+                },
             },
         },
     },
